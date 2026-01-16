d24af0b26c6536f5f710765155716f92b7c6a48d
wmwyy <1242422575@qq.com>
2026-01-16 21:29:43 +0800

    Delete snake_tencent directory

--- snake_tencent/backend/config.py
+++
@@ -1,9 +0,0 @@
-DEFAULT_SKIN = "Neon"
-SKINS = ["Neon", "Candy", "Metal"]
-DEFAULT_DIFFICULTY = "Normal"
-DIFFICULTY_SETTINGS = {
-    "Easy": {"speed": 4, "seedsToLevel": 6},
-    "Normal": {"speed": 6, "seedsToLevel": 5},
-    "Hard": {"speed": 8, "seedsToLevel": 4},
-}
-
